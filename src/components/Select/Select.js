a5f8b32ffd1258c7fb2ba93b0926d49a6889ff32
Maciek Sakrejda <m.sakrejda@gmail.com>
2022-12-21 17:22:18 -0800

    Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,42 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <StyledSelect value={value} onChange={onChange}>
+        {children}
+      </StyledSelect>
+      <Chevron id='chevron-down' size={24} strokeWidth={2} />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  width: fit-content;
+`;
+
+const Chevron = styled(Icon)`
+  color: ${COLORS.gray700};
+  pointer-events: none;
+  margin-top: auto;
+  margin-bottom: auto;
+  margin-left: -24px;
+`;
+
+const StyledSelect = styled.select`
+  appearance: none;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 4px;
+  border: none;
+  padding: 8px 8px;
+
+  padding-right: 36px;
+
+  &:hover {
+    color: ${COLORS.black};
+    outline: 1px solid ${COLORS.primary};
+  }
+`;
 
 export default Select;
